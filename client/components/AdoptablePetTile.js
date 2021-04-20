5fb0cd112970dd2d3d80e79ef1c0070cedc608b4
Christian <christiandecolombia99@gmail.com>
2021-04-20 15:50:06 -0700

    added alt tags to img as per Karren's request

--- client/components/AdoptablePetTile.js
+++ client/components/AdoptablePetTile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AdoptablePetTile = props => {
   let vaccination
-  if(props.vaccinationStatus) {
+  if (props.vaccinationStatus) {
     vaccination = "Yes"
   } else {
     vaccination = "No"
@@ -10,7 +10,7 @@ const AdoptablePetTile = props => {
 
   return (
     <div>
-      <img src={props.imgUrl}></img>
+      <img alt="Picture of an amimal" src={props.imgUrl}></img>
       <h2>Name: {props.name}</h2>
       <h3>Age: {props.age}</h3>
       <h3>Vaccinated: {vaccination}</h3>
