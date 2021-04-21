2aab40d9b5431b53f4dd2511cf55628918fc5ab6
JR <jrpark07@gmail.com>
2021-04-21 08:32:34 -0700

    new schema for adoption_application

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE adoptable_pets (
   available_for_adoption BOOLEAN DEFAULT true,
   pet_type_id INTEGER REFERENCES pet_types(id)
 );
+DROP TABLE IF EXISTS adoption_applications;
+CREATE TABLE adoption_applications (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone_number INTEGER NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  home_status VARCHAR(255) NOT NULL,
+  application_status VARCHAR(255) DEFAULT 'pending' NOT NULL,
+  adoptable_pet_id INTEGER REFERENCES adoptable_pets(id) 
+);
